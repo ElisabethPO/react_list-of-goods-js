a5adf6d50b185a72329e5e3f38c1f2d18e001acb
Elisabeth Polska <polskaelizaveta@gmail.com>
2025-03-18 16:45:16 +0200

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -14,33 +15,88 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" className="button is-info is-light">
-        Sort alphabetically
-      </button>
+export const App = () => {
+  const [sortField, setSortField] = useState('');
+  const [selectedAlphabet, setselectedAlphabet] = useState('');
+  const [selectedLength, setselectedLength] = useState('');
+  const [reversed, setReversed] = useState('');
 
-      <button type="button" className="button is-success is-light">
-        Sort by length
-      </button>
+  const visibleGoods = goodsFromServer.sort((good1, good2) => {
+    switch (sortField) {
+      case ''
+    }
+  })
 
-      <button type="button" className="button is-warning is-light">
-        Reverse
-      </button>
+  return (
+    <div className="section content">
+      <div className="buttons">
+        {goodsFromServer.map(field => (
+          <button
+            key={field}
+            type="button"
+            className="button is-info is-light"
+            onClick={() => setSortField(field)}
+          >
+            Sort alphabetically
+          </button>
 
-      <button type="button" className="button is-danger is-light">
-        Reset
-      </button>
+          <button type="button" className="button is-success is-light">
+            Sort by length
+          </button>
+
+          <button type="button" className="button is-warning is-light">
+            Reverse
+          </button>
+
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => setSortField('')}
+          >
+            Reset
+          </button>
+        ))}
+      </div>
+
+      <ul>
+        <li data-cy="Good">Dumplings</li>
+        <li data-cy="Good">Carrot</li>
+        <li data-cy="Good">Eggs</li>
+        <li data-cy="Good">Ice cream</li>
+        <li data-cy="Good">Apple</li>
+        <li data-cy="Good">...</li>
+      </ul>
     </div>
+  );
+};
 
-    <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
-    </ul>
-  </div>
-);
+// export const App = () => (
+//   <div className="section content">
+//     <div className="buttons">
+//       <button type="button" className="button is-info is-light">
+//         Sort alphabetically
+//       </button>
+
+//       <button type="button" className="button is-success is-light">
+//         Sort by length
+//       </button>
+
+//       <button type="button" className="button is-warning is-light">
+//         Reverse
+//       </button>
+
+//       <button type="button" className="button is-danger is-light">
+//         Reset
+//       </button>
+//     </div>
+
+//     <ul>
+//       <li data-cy="Good">Dumplings</li>
+//       <li data-cy="Good">Carrot</li>
+//       <li data-cy="Good">Eggs</li>
+//       <li data-cy="Good">Ice cream</li>
+//       <li data-cy="Good">Apple</li>
+//       <li data-cy="Good">...</li>
+//     </ul>
+//   </div>
+// );
